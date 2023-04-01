a0569d2a847ab3397706d2ce5f2730c410a756e7
BVishal-Geek <bakshiv193@gmail.com>
2023-04-01 19:19:38 +0530

    ReadImage

--- ReadWriteImage.py
+++ ReadWriteImage.py
@@ -1,1 +1,11 @@
 import cv2 
+import sys  
+
+img = cv2.imread(cv2.samples.findFile(r"apple.jpg"))
+path = cv2.samples.findFile(r"apple.jpg")
+print(path)
+if img is None:
+    sys.exit("Image is not present")
+
+cv2.imshow("Window", img)
+cv2.waitKey(0)
